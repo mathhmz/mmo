c3bdbe47ee6ad7fae5d8c6344182c99b9827b2cf
math_hmz <rm551843@fiap.com.br>
2024-03-04 22:09:29 -0300

    :sparkles: feat: Server side chat handling

--- server/protocol.py
+++ server/protocol.py
@@ -1,59 +1,61 @@
 import queue
 import packet
-
 from autobahn.twisted.websocket import WebSocketServerProtocol
 
+
 class GameServerProtocol(WebSocketServerProtocol):
-    
     def __init__(self):
         super().__init__()
         self._packet_queue: queue.Queue[tuple['GameServerProtocol', packet.Packet]] = queue.Queue()
         self._state: callable = self.PLAY
-        
+
     def PLAY(self, sender: 'GameServerProtocol', p: packet.Packet):
-        pass
-    
+        if p.action == packet.Action.Chat:
+            if sender == self:
+                self.broadcast(p, exclude_self=True)
+            else:
+                self.send_client(p)
+
     def tick(self):
-        
-        if not self.packet_queue.empty():
+        # Process the next packet in the queue
+        if not self._packet_queue.empty():
             s, p = self._packet_queue.get()
             self._state(s, p)
-            
-    def broadcast(self, p: packet.Packet, exclude_self:bool = False):
+
+    def broadcast(self, p: packet.Packet, exclude_self: bool = False):
         for other in self.factory.players:
             if other == self and exclude_self:
                 continue
             other.onPacket(self, p)
-            
-    def onPacket(self, sender: 'GameServerProtocol', p: packet.Packet):
-        self._packet_queue.put((sender,p))
-        print(f'Queued Packet: {p}')
-        
-        
-    #Overrides
-    
+
+    # Override
     def onConnect(self, request):
         print(f"Client connecting: {request.peer}")
-    
+
+    # Override
     def onOpen(self):
-        print(f'Websocket connection open')
-        self._state = self.PLAY
-        
+        print(f"Websocket connection open.")
+
+    # Override
     def onClose(self, wasClean, code, reason):
         self.factory.players.remove(self)
-        print(f'Websocket connection closed{"Unexpectadly" if not wasClean else "Cleanly"} with code {code} : {reason}')
-        
-        
+        print(f"Websocket connection closed{' unexpectedly' if not wasClean else ' cleanly'} with code {code}: {reason}")
+
+    # Override
     def onMessage(self, payload, isBinary):
-        decoded_payload = payload.decode('utf-9')
-        
+        decoded_payload = payload.decode('utf-8')
+
         try:
             p: packet.Packet = packet.from_json(decoded_payload)
         except Exception as e:
             print(f"Could not load message as packet: {e}. Message was: {payload.decode('utf8')}")
-            
+
         self.onPacket(self, p)
-        
-    def send_client(self, p:packet.Packet):
+
+    def onPacket(self, sender: 'GameServerProtocol', p: packet.Packet):
+        self._packet_queue.put((sender, p))
+        print(f"Queued packet: {p}")
+
+    def send_client(self, p: packet.Packet):
         b = bytes(p)
         self.sendMessage(b)
